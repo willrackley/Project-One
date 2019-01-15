184b547283fd36be1d518c82be1b86339f5fb2d8
Justas <justaslau@gmail.com>
2019-01-14 20:01:46 -0500

    added nutritions search by product id

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,21 +4,17 @@ Title: Complete Welness Coach
 Descreption: Georgia Tech Bootcamp: Project 1
 Date: 2019-01-10
 */
-
 $("#moreInfoFormContainer").hide();
 //this click event is for the 'what is your goals form
 $("#goalsGoButton").on("click", function() {
 	event.preventDefault();
 	$("#moreInfoFormContainer").fadeIn(2000);
 });
-
-
 var foodApiKey = "rMQ7M66s5OQxDWwDt3XXLIJQEjVxE1JHOGpKvq7r";
 var productName = "";
 var dataSource = "Standard+Reference";
 var prodChoices = [];
 var queryURL = "https://api.nal.usda.gov/ndb/search/?offset=150&q=" + productName + "&api_key=" + foodApiKey + "&ds=" + dataSource;
-
 // Function to get all products names localy from API to prepare autocomplete input.
 function getProducts() {
 	$.ajax({
@@ -38,21 +34,17 @@ function getProducts() {
 		}
 	});
 }
-
 // Function to find product nutritions by product ID
-function getNutrition (productId) {
-    var queryURL = "https://api.nal.usda.gov/ndb/nutrients/?max=1&nutrients=208&ndbno=" + productId + "&api_key=" + foodApiKey;
-    console.log(queryURL);
-    $.ajax({
+function getNutrition(productId) {
+	var queryURL = "https://api.nal.usda.gov/ndb/nutrients/?max=1&nutrients=208&ndbno=" + productId + "&api_key=" + foodApiKey;
+	console.log(queryURL);
+	$.ajax({
 		url: queryURL,
 		method: "GET"
 	}).then(function(response) {
-        console.log(response.report.foods[0]);
-    });
-
-
+		console.log(response.report.foods[0]);
+	});
 }
-
 // Input Autocomplete by product name
 $('input[name="q"]').autoComplete({
 	minChars: 2,
@@ -64,22 +56,16 @@ $('input[name="q"]').autoComplete({
 		suggest(matches);
 	}
 });
-
 $("#submit-product").on("click", function() {
-    var usersProduct = $("#enter-product").val().trim();
-    console.log(usersProduct);
-    var queryURL = "https://api.nal.usda.gov/ndb/search/?max=1&q=" + usersProduct + "&api_key=" + foodApiKey + "&ds=" + dataSource;
-    $.ajax({
+	var usersProduct = $("#enter-product").val().trim();
+	console.log(usersProduct);
+	var queryURL = "https://api.nal.usda.gov/ndb/search/?max=1&q=" + usersProduct + "&api_key=" + foodApiKey + "&ds=" + dataSource;
+	$.ajax({
 		url: queryURL,
 		method: "GET"
-    }).then(function(response) {
-        console.log(response.list.item[0].ndbno);
-        getNutrition(response.list.item[0].ndbno);
-    });
-    
-    
-
+	}).then(function(response) {
+		console.log(response.list.item[0].ndbno);
+		getNutrition(response.list.item[0].ndbno);
+	});
 });
-
-
 getProducts();
