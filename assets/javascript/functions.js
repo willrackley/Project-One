c1080a6773f859e987e4efcf8383dbf65a8bd227
Creative Hot Mess <nmsuri28@gmail.com>
2019-01-15 20:48:15 -0800

    edited javascript

--- assets/javascript/functions.js
+++ assets/javascript/functions.js
@@ -1,11 +1,8 @@
-$("#age").val();
-console.log("#age");
-
 
 //onclick function
 $("#calculate").click(function(){
     console.log("This button was clicked.");
     var age=parseFloat($("#age").val().trim());
+    var weight=parseFloat($("#weight").val().trim());
     console.log(age);
-
 });
